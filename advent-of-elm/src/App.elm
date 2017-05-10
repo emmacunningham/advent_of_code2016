18d4adad775d2e074c904f833928023f7fea7376
Emma Cunningham <emma@secondspectrum.com>
2017-05-10 07:52:31 -0700

    Add Project Euler #2 solution

--- advent-of-elm/src/App.elm
+++ advent-of-elm/src/App.elm
@@ -2,58 +2,135 @@ module App exposing (..)
 
 import Html exposing (Html, text, div, img)
 import Html.Attributes exposing (src)
+import Http
 
 
 type alias Model =
     { message : String
-    , logo : String
-    , challenges : List Challenge
+    , solution : String
     }
 
 
 type alias Challenge =
-    { input : String
-    , solveFn : String -> String
+    { solution : String
     , name : String
     }
+
+
+challenges : List Challenge
+challenges =
+    [ { name = "Project Euler 1: "
+      , solution = toString (computeFizzBuzzSum 1000)
+      }
+    , { name = "Project Euler 2: "
+      , solution = toString (computeFiboSum 4000000 1 2 0)
+      }
+    ]
 
 
 init : String -> ( Model, Cmd Msg )
 init path =
     ( { message = "Advent of Elm"
-      , logo = path
-      , challenges =
-            [ { input = "day5.txt"
-              , solveFn = \x -> x
-              , name = "Day 5"
-              }
-            ]
+      , solution =
+            ""
+            -- , challenges =
+            --       [ { input = "day6.txt"
+            --         , solveFn = \x -> x
+            --         , name = "Day 6"
+            --         }
+            --       ]
       }
-    , Cmd.none
+    , fetchData
     )
 
 
 type Msg
-    = NoOp
+    = Day6Data (Result Http.Error String)
+
+
+fetchData : Cmd Msg
+fetchData =
+    let
+        request =
+            Http.getString "input/day6.txt"
+    in
+        Http.send Day6Data request
+
+
+generateAcc : Int -> List (List String)
+generateAcc num =
+    let
+        listRange =
+            List.range 0 (num - 1)
+    in
+        List.map (\x -> []) listRange
+
+
+foldLines : Int -> String -> List (List String) -> List (List String)
+foldLines lineLength val list =
+    let
+        v =
+            Debug.log "v" val
+
+        l =
+            Debug.log "v" list
+    in
+        [ [ val ] ]
+
+
+transposeList : List String -> Int -> List String
+transposeList lines idx =
+    List.map (\x -> String.slice (idx - 1) idx x) lines
+
+
+updateModel : Model -> String -> Model
+updateModel model response =
+    { model | solution = "foo" }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        Day6Data (Ok response) ->
+            ( updateModel model response, Cmd.none )
+
+        Day6Data (Err _) ->
+            ( model, Cmd.none )
 
 
 renderChallenge : Challenge -> Html Msg
 renderChallenge challenge =
-    div [] [ text challenge.name ]
+    div [] [ text challenge.name, text challenge.solution ]
+
+
+computeFizzBuzzSum : Int -> Int
+computeFizzBuzzSum upperBound =
+    List.range 0 (upperBound - 1)
+        |> List.filter (\x -> x % 3 == 0 || x % 5 == 0)
+        |> List.sum
+
+
+computeFiboSum : Int -> Int -> Int -> Int -> Int
+computeFiboSum upperBound f1 f2 sum =
+    let
+        nextSum =
+            case f1 % 2 of
+                0 ->
+                    sum + f1
+
+                _ ->
+                    sum
+    in
+        if f2 > upperBound then
+            nextSum
+        else
+            computeFiboSum upperBound f2 (f1 + f2) nextSum
 
 
 view : Model -> Html Msg
 view model =
     div []
-        -- [ img [ src model.logo ] []
-        -- , div [] [ text model.message ]
-        -- ]
-        (List.map renderChallenge model.challenges)
+        (List.map renderChallenge challenges)
 
 
 subscriptions : Model -> Sub Msg
