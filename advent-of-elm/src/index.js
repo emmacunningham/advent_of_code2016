7c0ae4e9cb6beb360b072b6e5035c56d391d8a28
Emma Cunningham <emma@secondspectrum.com>
2017-06-14 07:57:45 -0700

    Add vanilla JS solution for Product of Others

--- advent-of-elm/src/index.js
+++ advent-of-elm/src/index.js
@@ -5,3 +5,22 @@ var Elm = require('./Main.elm');
 var root = document.getElementById('root');
 
 Elm.Main.embed(root, logoPath);
+
+
+// The vanilla JS solution to Interview Cake's Product of Others problem
+
+function listProduct(arr) {
+  return arr.reduce((acc, val) => acc * val);
+}
+
+function getProductOfOthers(arr) {
+  const result = []  
+  for (let i = 0; i < arr.length; i++) {
+    const others = arr.filter((elm, idx, _) => idx !== i);
+    result.push(listProduct(others))    
+  }
+
+  return result;
+}
+
+console.log(getProductOfOthers([1, 7, 3, 4]));
